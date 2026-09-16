60b6a3700b160d117c4dd6e5830f316db6134748
PrinceKr786 <princekumarjamui786@gmail.com>
2026-09-16 22:28:35 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCYoVCor_9EJ5JSIMganWTOhbHdUNa5rfM",
-    authDomain: "brutalpanel-ec8a0.firebaseapp.com",
-    databaseURL: "https://brutalpanel-ec8a0-default-rtdb.firebaseio.com",
-    projectId: "brutalpanel-ec8a0",
-    storageBucket: "brutalpanel-ec8a0.firebasestorage.app",
-    messagingSenderId: "276068222384",
-    appId: "1:276068222384:web:96b75c5877b921f3f40275",
-    measurementId: "G-ZSLZS6K781"
+    apiKey: "AIzaSyDLX-MlSG_BlTNO9ZIaIniD1QCPIodGOo8",
+    authDomain: "paid-16761.firebaseapp.com",
+    databaseURL: "https://paid-16761-default-rtdb.firebaseio.com",
+    projectId: "paid-16761",
+    storageBucket: "paid-16761.firebasestorage.app",
+    messagingSenderId: "949226899902",
+    appId: "1:949226899902:web:9e26aca139f079fa3e51ef",
+    measurementId: "G-3N5SG370Z9"
 };
 
 const app = initializeApp(firebaseConfig);
